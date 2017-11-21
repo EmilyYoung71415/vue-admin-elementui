22777f5422f650448985c732eb3ad02c810e5707
YXY <775363160@qq.com>
2017-11-21 19:54:37 +0800

    引入gulp-css-wrap神器

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var path = require('path')
+var gulp = require('gulp')
+var cleanCSS = require('gulp-clean-css')
+var cssWrap = require('gulp-css-wrap')
+gulp.task('css-wrap', function () {
+	return gulp.src(path.resolve('./theme/index.css'))/* 找需要添加命名空间的css文件，支持正则表达式 */
+		.pipe(cssWrap({
+			selector: '.custom-1b1e24' /* 添加的命名空间 */
+		}))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('src/assets/css/theme/1b1e24')) /* 存放的目录 */
+})
